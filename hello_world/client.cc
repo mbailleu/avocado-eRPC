f92770b593bc75806432ce1666da21f978e9118e
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-01-28 12:41:34 -0500

    modify hello_world to correspond to test

--- hello_world/client.cc
+++ hello_world/client.cc
@@ -1,27 +1,37 @@
-#include "common.h"
+#include "rpc.h"
+
 erpc::Rpc<erpc::CTransport> *rpc;
 erpc::MsgBuffer req;
 erpc::MsgBuffer resp;
 
-void cont_func(void *, void *) { printf("%s\n", resp.buf); }
+void cont_func(void *, void *) {
+  assert(resp.get_data_size() == 16);
+
+  // The response has its first 16 bytes set to 'p'. The server sets the 17th
+  // byte to 'q', but the client does not receive that byte.
+  for (size_t i = 0; i <= 16; i++) {
+    printf("%zu: %c\n", i, resp.buf[i]);
+    if (i < 16) assert(resp.buf[i] == 'p');
+  }
+  printf("\n");
+}
 
 void sm_handler(int, erpc::SmEventType, erpc::SmErrType, void *) {}
 
 int main() {
-  std::string client_uri = kClientHostname + ":" + std::to_string(kUDPPort);
-  erpc::Nexus nexus(client_uri, 0, 0);
-
+  erpc::Nexus nexus("localhost:31850", 0, 0);
   rpc = new erpc::Rpc<erpc::CTransport>(&nexus, nullptr, 0, sm_handler);
 
-  std::string server_uri = kServerHostname + ":" + std::to_string(kUDPPort);
-  int session_num = rpc->create_session(server_uri, 0);
-
+  int session_num = rpc->create_session("localhost:31851", 0);
   while (!rpc->is_connected(session_num)) rpc->run_event_loop_once();
 
-  req = rpc->alloc_msg_buffer_or_die(kMsgSize);
-  resp = rpc->alloc_msg_buffer_or_die(kMsgSize);
+  req = rpc->alloc_msg_buffer_or_die(32);
+  resp = rpc->alloc_msg_buffer_or_die(16400);
+  assert(req.get_data_size() == 32);
+  assert(resp.get_data_size() == 16400);
 
-  rpc->enqueue_request(session_num, kReqType, &req, &resp, cont_func, nullptr);
+  rpc->enqueue_request(session_num, 9 /* req_type */, &req, &resp, cont_func,
+                       nullptr);
   rpc->run_event_loop(100);
 
   delete rpc;
--- hello_world/server.cc
+++ hello_world/server.cc
@@ -1,18 +1,21 @@
-#include "common.h"
+#include "rpc.h"
+
 erpc::Rpc<erpc::CTransport> *rpc;
 
 void req_handler(erpc::ReqHandle *req_handle, void *) {
   auto &resp = req_handle->pre_resp_msgbuf;
-  rpc->resize_msg_buffer(&resp, kMsgSize);
-  sprintf(reinterpret_cast<char *>(resp.buf), "hello");
+  rpc->resize_msg_buffer(&resp, 16);
 
+  assert(resp.get_data_size() == 16);
+
+  memset(resp.buf, 'p', 16);  // Client will see first 16 B of response = 'p'
+  resp.buf[16] = 'q';         // Client won't see this byte
   rpc->enqueue_response(req_handle, &resp);
 }
 
 int main() {
-  std::string server_uri = kServerHostname + ":" + std::to_string(kUDPPort);
-  erpc::Nexus nexus(server_uri, 0, 0);
-  nexus.register_req_func(kReqType, req_handler);
+  erpc::Nexus nexus("localhost:31851", 0, 0);
+  nexus.register_req_func(9 /* req type */, req_handler);
 
   rpc = new erpc::Rpc<erpc::CTransport>(&nexus, nullptr, 0, nullptr);
   rpc->run_event_loop(100000);
